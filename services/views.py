b8aa966fceafb2ef617fc167046e0f5937efd970
ThomG1 <thomas.gwyther@hotmail.co.uk>
2023-05-15 09:36:55 +0000

    Add services admin

--- services/views.py
+++ services/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_services(request):
+    """ A view to show all services, including sorting and search queries """
+
+    services = Product.objects.all()
+
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'services/services.html', context)
