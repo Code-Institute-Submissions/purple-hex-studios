3f8d991af2bef1af039ee76893bbb117398e13bf
ThomG1 <thomas.gwyther@hotmail.co.uk>
2023-06-04 09:57:34 +0000

    Added defensive programming

--- services/views.py
+++ services/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Product, Category
 
@@ -50,8 +51,13 @@ def service_detail(request, product_id):
 
     return render(request, 'services/service_detail.html', context)
 
+@login_required
 def add_service(request):
     """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -70,8 +76,13 @@ def add_service(request):
 
     return render(request, template, context)
 
+@login_required
 def edit_service(request, product_id):
     """ Edit a service in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     service = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=service)
@@ -93,8 +104,13 @@ def edit_service(request, product_id):
 
     return render(request, template, context)
 
+@login_required
 def delete_service(request, product_id):
     """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+        
     service = get_object_or_404(Product, pk=product_id)
     service.delete()
     messages.success(request, 'Service deleted')
