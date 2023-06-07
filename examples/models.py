1f9c924d52d5b0785c961b689e69d63114089052
ThomG1 <thomas.gwyther@hotmail.co.uk>
2023-06-07 14:33:14 +0000

    Update docstring

--- examples/models.py
+++ examples/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Example(models.Model):
     """
-    Example model.
+    Example model
     """
     name = models.CharField(max_length=150)
     image = models.ImageField(upload_to='examples')
