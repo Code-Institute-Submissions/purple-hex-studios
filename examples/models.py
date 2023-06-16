59188cf3c200c45e3410bc13883c75e21437524c
ThomG1 <thomas.gwyther@hotmail.co.uk>
2023-06-16 23:18:35 +0000

    Add blank line

--- examples/models.py
+++ examples/models.py
@@ -8,7 +8,6 @@ class Example(models.Model):
     name = models.CharField(max_length=150)
     image = models.ImageField(upload_to='examples')
     link = models.URLField(max_length=200)
-    
 
     def __str__(self):
         return self.name
