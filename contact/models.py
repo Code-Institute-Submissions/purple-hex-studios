27b88592742e189142838d4aaaf9dd84945f693b
ThomG1 <thomas.gwyther@hotmail.co.uk>
2023-06-16 23:04:18 +0000

    Remove whitespace

--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Contact(models.Model):
-     name = models.CharField(max_length=100, null=False, blank=False)
-     email = models.EmailField(null=False, blank=False)
-     message = models.TextField(null=False, blank=False)
- 
-     def __str__(self):
-         return self.name
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
