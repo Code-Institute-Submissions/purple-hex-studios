2f5a2e5b9cbeba8f26b286e80e404605a9d975cc
ThomG1 <thomas.gwyther@hotmail.co.uk>
2023-06-16 12:46:39 +0000

    Fix indentation

--- services/urls.py
+++ services/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.all_services, name='services'),
-    path('detail/<int:service_id>/', views.service_detail, name='service_detail'),
+    path('detail/<int:service_id>/',
+         views.service_detail,
+         name='service_detail'),
     path('add/', views.add_service, name='add_service'),
-    path('delete/<int:service_id>/', views.delete_service, name='delete_service'),
+    path('delete/<int:service_id>/',
+         views.delete_service,
+         name='delete_service'),
     path('edit/<int:service_id>/', views.edit_service, name='edit_service'),
 ]
